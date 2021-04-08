5690c79bcd6327e7df10517598ee20005536cd87
congle98 <lethanhcongcnhn@gmail.com>
2021-04-08 15:12:04 +0700

    addfile

---
+++ src/demo/demo.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class demo {
+    public static void main(String[] args) {
+        Point3D point3D =new Point3D();
+        point3D.setXYZ(8,10,12);
+        System.out.println(point3D);
+    }
+
+}
